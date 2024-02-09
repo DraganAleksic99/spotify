f0ada81cd19695b9230dc5a7788ec63761af831b
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-02-09 19:34:59 +0100

    add library section

---
+++ src/app/components/LeftsideFooter.tsx
@@ -0,0 +1,3 @@
+export default function LeftsideFooter() {
+  return <div>Footer</div>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/Navigation";
+import YourLibrary from "./components/YourLibrary";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,24 +22,27 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="h-screen p-2">
           <div className="main h-[90%] rounded-lg flex gap-2">
-            <div className="secondary w-[23%]">
+            <div className="secondary w-[23%] h-full">
               <Navigation />
+              <YourLibrary />
             </div>
             <div className="primary w-[77%] rounded-lg bg-[#121212]">
               {children}
             </div>
           </div>
-          <div className="footer flex justify-between pl-[15px] pt-[11px] pb-[7px] pr-[24px] bg-red-700 h-[9%] mt-2 bg-gradient-to-r from-[#af2896] to-[#509bf5] text-[14px]">
+          <div className="footer flex justify-between pl-[15px] py-2 pr-[24px] bg-red-700 h-[9%] mt-2 bg-gradient-to-r from-[#af2896] to-[#509bf5] text-[14px]">
             <div className="mt-1">
-              <p className="font-bold text-sm">Preview of Spotify</p>
-              <p>
+              <p className="font-bold">Preview of Spotify</p>
+              <p className="text-base">
                 Sign up to get unlimited songs and podcasts with occasional ads.
                 No credit card needed.
               </p>
             </div>
             <div>
               <button className="bg-white text-black h-full rounded-full">
-                <span className="py-2 px-8 font-bold">Sign up for free</span>
+                <span className="py-2 px-8 font-bold text-base">
+                  Sign up for free
+                </span>
               </button>
             </div>
           </div>
